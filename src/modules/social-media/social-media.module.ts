3d4f9cdab19b24207906bd1628cc3b1a02e62f82
Alireza Sharifpour <ascode94@gmail.com>
2025-06-02 15:39:13 +0330

    Integrate SocialMediaModule into AppModule and enhance SocialMediaModule with ConfigModule and CacheModule for improved configuration and caching capabilities.

--- src/modules/social-media/social-media.module.ts
+++ src/modules/social-media/social-media.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '../../core/config/config.module';
+import { CacheModule } from '../../core/cache/cache.module';
 import { TwitterService, FarcasterService } from './services';
 
 @Module({
   imports: [
     HttpModule, // For making HTTP requests to external APIs (Apify, Searchcaster, Warpcast)
+    ConfigModule,
+    CacheModule,
   ],
   providers: [TwitterService, FarcasterService],
   exports: [TwitterService, FarcasterService],

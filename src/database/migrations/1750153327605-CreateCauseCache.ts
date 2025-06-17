51d535942ea4a7a546deef5066a8c77ee8bf2e24
Alireza Sharifpour <ascode94@gmail.com>
2025-06-17 13:24:46 +0330

    Create migration for cause_cache table with indexing support
    This commit introduces a new migration that creates the "cause_cache" table, which includes fields for id, title, description, project_ids, cached_at, and expires_at. An index is also created on the expires_at field to optimize query performance. The migration includes both up and down methods for applying and reverting the changes, ensuring proper database management.

---
+++ src/database/migrations/1750153327605-CreateCauseCache.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateCauseCache1750153327605 implements MigrationInterface {
+  name = 'CreateCauseCache1750153327605';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "cause_cache" ("id" integer NOT NULL, "title" character varying(255) NOT NULL, "description" text NOT NULL, "project_ids" jsonb NOT NULL, "cached_at" TIMESTAMP NOT NULL DEFAULT now(), "expires_at" TIMESTAMP NOT NULL, CONSTRAINT "PK_cbbb5f1b866601ac4bad013982b" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_e9879f87c3eedf70548f93eae8" ON "cause_cache" ("expires_at") `,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_e9879f87c3eedf70548f93eae8"`,
+    );
+    await queryRunner.query(`DROP TABLE "cause_cache"`);
+  }
+}

b568cc190a543de82cb463968631e48d5074cdef
Alireza Sharifpour <ascode94@gmail.com>
2025-06-18 13:33:43 +0330

    update jobs module

--- src/modules/scheduled-jobs/scheduled-jobs.module.ts
+++ src/modules/scheduled-jobs/scheduled-jobs.module.ts
@@ -2,18 +2,31 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SocialMediaStorageModule } from '../social-media-storage/social-media-storage.module';
 import { SocialMediaModule } from '../social-media/social-media.module';
+import { DataFetchingModule } from '../data-fetching/data-fetching.module';
 import { ScheduledJob } from '../social-media-storage/entities/scheduled-job.entity';
 import { JobSchedulerService } from './services/job-scheduler.service';
 import { JobProcessorService } from './services/job-processor.service';
 import { TwitterFetchProcessor } from './processors/twitter-fetch.processor';
+import { ProjectSyncProcessor } from './processors/project-sync.processor';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ScheduledJob]),
     SocialMediaStorageModule,
     SocialMediaModule,
+    DataFetchingModule,
   ],
-  providers: [JobSchedulerService, JobProcessorService, TwitterFetchProcessor],
-  exports: [JobSchedulerService, JobProcessorService, TwitterFetchProcessor],
+  providers: [
+    JobSchedulerService,
+    JobProcessorService,
+    TwitterFetchProcessor,
+    ProjectSyncProcessor,
+  ],
+  exports: [
+    JobSchedulerService,
+    JobProcessorService,
+    TwitterFetchProcessor,
+    ProjectSyncProcessor,
+  ],
 })
 export class ScheduledJobsModule {}
